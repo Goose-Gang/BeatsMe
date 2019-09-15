b769ddbe09851a95badfc2189746d8424f502bce
Andrew Yang <andrew.yang.school@gmail.com>
2019-09-15 04:23:02 -0400

    Write Routes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@
 
 import 'package:beats_me/collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,69 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'SF Pro Display'),
       home: Collection(),
+    );
+  }
+}
+
+// Widget to capture the audio
+class AudioLoad extends StatefulWidget {
+  createState() => _AudioLoadState();
+}
+
+class _AudioLoadState extends State<AudioLoad> {
+  // Active audio file
+  File _audioFile;
+
+  // Select audio via gallery or camera
+  Future<void> _pickAudio(ImageSource source) async {
+    File selected = await FilePicker.pickAudio(source: source);
+
+    setState(() {
+      _audioFile = selected;
+    });
+  }
+
+  // Remove audio
+  void _clear() {
+    setState(() => _audioFile = null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      // Select audio from gallery
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.photo_library),
+              onPressed: () => _pickAudio(ImageSource.gallery),
+            ),
+          ],
+        ),
+      ),
+
+      // Preview the audio
+      body: ListView(
+        children: <Widget>[
+          if (_audioFile != null) ...[
+
+            File.file(_audioFile),
+
+            Row(
+              children: <Widget>[
+                FlatButton(
+                  child: Icon(Icons.refresh),
+                  onPressed: _clear,
+                ),
+              ],
+            ),
+
+            Uploader(file: _audioFile)
+          ]
+        ],
+      ),
     );
   }
 }
